ee391aa75244e7839563ec7ddb8d3cedc0174ac1
Montassar Gharbi <gharbimontassar2002gmail.com>
2026-05-03 14:52:47 +0100

    init: initialisation du projet Spring Boot

---
+++ src/test/java/com/projet/gestionusers/GestionuserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.gestionusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionuserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
