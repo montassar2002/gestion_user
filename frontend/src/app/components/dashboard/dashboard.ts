062dd8cfe97cc0a49ddb56eb2d7a71898f6a85eb
Montassar Gharbi <gharbimontassar2002gmail.com>
2026-05-11 11:45:14 +0100

    feat: ajout composants utilisateurs, roles, permissions, historique

--- frontend/src/app/components/dashboard/dashboard.ts
+++ frontend/src/app/components/dashboard/dashboard.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { UtilisateurService } from '../../services/utilisateur';
+import { RoleService } from '../../services/role';
+import { PermissionService } from '../../services/permission';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
-  templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './dashboard.html'
 })
-export class Dashboard {}
+export class Dashboard implements OnInit {
+  totalUtilisateurs = 0;
+  totalRoles = 0;
+  totalPermissions = 0;
+  totalActifs = 0;
+
+  constructor(
+    private utilisateurService: UtilisateurService,
+    private roleService: RoleService,
+    private permissionService: PermissionService
+  ) {}
+
+  ngOnInit() {
+    this.utilisateurService.getAll().subscribe(data => {
+      this.totalUtilisateurs = data.length;
+    });
+    this.utilisateurService.getActifs().subscribe(data => {
+      this.totalActifs = data.length;
+    });
+    this.roleService.getAll().subscribe(data => {
+      this.totalRoles = data.length;
+    });
+    this.permissionService.getAll().subscribe(data => {
+      this.totalPermissions = data.length;
+    });
+  }
+}
